70a150aed84228eb27bdeb3d235e877d4cbe6b72
michalbogaczynski <bogaczynski.m@gmail.com>
2019-01-13 20:27:37 +0100

    Dziedziczenie

---
+++ Dziedziczenie/Jewels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziedziczenie
+{
+    class Jewels
+    {
+        public string Sparkle()
+        {
+            return "Lśnimy i błyszczymy";
+        }
+    }
+}
